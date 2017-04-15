9122c1e69b83f1d1ac6905cf364e102380f2eab7
Sagar Manohar <sagarmanohar24@gmail.com>
2017-04-15 16:49:40 -0400

    Added murmurhash

--- bloom.c
+++ bloom.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdint.h>
 #include <math.h>
 
 #define k 8
@@ -8,31 +8,50 @@
 #define SetBit(A,k) ( A[(k/32)] |= (1 << (k%32)) )
 #define GetBit(A,k) ( A[(k/32)] & (1 << (k%32)) )
 
+uint32_t murmur3_32(const char* key, size_t len, uint32_t seed);
+
 typedef struct bf_t {
 	int numHashes;
-	int hashes[k];
+	int *hashes[k];
 } bf_t;
 
 bf_t * create_bf(){
+	int i;
 	bf_t *bloom = (bf_t *) malloc(sizeof(bf_t));
 	bloom->numHashes = k;
-	for(int i = 0; i < k; i++){
+	for(i = 0; i < k; i++){
 		bloom->hashes[i] = calloc(ARRAY_SIZE, sizeof(int));
 	}
 	return bloom;
 }
 
 void insert_bf(bf_t *b, char *s){
-
+	int i;
+	uint32_t h;
+	for(i = 0; i < b->numHashes; i++){
+		h = murmur3_32(s, sizeof(char), (i + 1) * 10);
+		SetBit(b->hashes[i], h % ARRAY_SIZE);
+	}
 }
 
 int is_element(bf_t *b, char *q){
-	if() {
-		return 1;
-	} else return 0;
+	int i;
+	uint32_t h;
+	for(i = 0; i < b->numHashes; i++){
+		h = murmur3_32(q, sizeof(char), (i + 1) * 10);
+		if(!GetBit(b->hashes[i], h % ARRAY_SIZE)){
+			return 0;
+		}
+	}
+	return 1;
 }
 
 int main() {
-	char *a = calloc(10, sizeof(char));
-	int b = a[500];
+	bf_t *b = create_bf();
+	insert_bf(b, "World");
+	insert_bf(b, "Good");
+	int a = is_element(b, "Hello");
+	int bc = is_element(b, "World");
+	int c = is_element(b, "Good");
+	printf("%d, %d, %d", a, bc, c);
 }
